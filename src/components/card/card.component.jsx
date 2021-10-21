ea420c5a88fd956825e6a5f9c682537c168c9a79
Filip Ragnestål <filipragnestal@filips-mbp.home>
2021-10-21 21:00:57 +0200

    filer for github pages

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./card.style.css";
+
+export const Card = (props) => (
+  <div className="card-container">
+    <img
+      alt="monster"
+      src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+    />
+    <h2>{props.monster.name}</h2>
+    {/* <h1> {props.monster.username}</h1> */}
+    <p> 
+      { props.monster.email}
+    </p>
+  </div>
+);
+
+// https://robohash.org/${props.monster.id}?set=set2&size=180x180  https://robohash.org/1?set=set2
